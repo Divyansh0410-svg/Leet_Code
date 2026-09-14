ba0fcfd38a9b6fc4e8c204b551d30c1e42cdb284
Divyansh0410-svg <divyanshpersonal2008@gmail.com>
2026-09-14 14:35:34 +0530

    Time: 0 ms (100%), Space: 16.7 MB (95.33%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,16 +1,16 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(2 * n);
 
-        int n=nums.size();
-        vector<int> ans={};
-
-        for(int num:nums){
-            ans.push_back(num);
-        }
-        
-        for(int num:nums){
-            ans.push_back(num);
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];       
+            ans[i + n] = nums[i];   
         }
 
         return ans;
